3d46785f0b9f8d5abcbc87fd2c8d66bb68e2ba8e
Eddie <forest.lithuania@gmail.com>
2019-06-25 18:13:36 +0100

    Final

--- app.py
+++ app.py
@@ -182,4 +182,5 @@ def get_ingredients_max_value():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
